2266e90c58a666765ae6d0ba3e437366ff42618e
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2026-02-06 16:23:02 -0300

    feat: implement pagination component and service; integrate into home page

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -3,21 +3,34 @@ import { rxResource } from '@angular/core/rxjs-interop';
 import { FrontNavbar } from '../../components/front-navbar/front-navbar/front-navbar';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { ProductService } from '@products/services/products.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 // import { ProductCard } from '../../../products/components/product-card/product-card';
 
 @Component({
   selector: 'app-home-page',
-  imports: [FrontNavbar, ProductCard],
+  imports: [FrontNavbar, ProductCard, PaginationComponent],
   templateUrl: './home-page.html',
 })
 export class HomePage {
   productsService = inject(ProductService);
+  paginationService = inject(PaginationService);
+  // activatedRoute = inject(ActivatedRoute);
+
+  // currentPage = toSignal(
+  //   this.activatedRoute.queryParamMap.pipe(
+  //     map((params) => (params.get('page') ? +params.get('page')! : 1)),
+  //     map((page) => (isNaN(page) ? 1 : page)),
+  //   ),
+  // );
 
   productsResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.paginationService.currentPage()! - 1 }),
     stream: ({ params }) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: params.page ? params.page * 9 : 0,
+      });
     },
   });
 }
